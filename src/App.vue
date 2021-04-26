98b882928c85059b4264109569f05211b426fdab
thatkidnamedrox <roxanne.harris@yale.edu>
2021-04-26 16:57:30 -0400

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,22 @@
   <div id="app">
     <img alt="Display" src="./assets/display.png">
     <PeopleCounter/>
+    <PersonMood/>
   </div>
 </template>
 
 <script>
 import PeopleCounter from './components/PeopleCounter.vue'
+import PersonMood from './components/PersonMood.vue'
 
 export default {
   name: 'App',
   components: {
-    PeopleCounter
+    PeopleCounter,
+    PersonMood
   }
 }
+
 </script>
 
 <style>
---
+++ src/components/PersonMood.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <!-- this variable, this.$store.state.num_people is defined in main.js in the vuex store -->
+    <div>{{this.$store.state.mood}}</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'PersonMood',
+}
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #eefcf5;
+}
+</style>
